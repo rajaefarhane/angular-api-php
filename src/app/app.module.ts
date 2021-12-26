cce54c9c0fca70d7f7f83b459268b702ca97c260
user.name <r.farhane@codeur.online>
2021-12-26 10:54:36 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ViewComponent } from './view/view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
